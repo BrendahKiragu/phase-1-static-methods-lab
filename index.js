4a061eb07408e8dda05de30902b4e2ff5de02f31
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-25 22:02:19 +0300

    adds a static method titlelize

--- index.js
+++ index.js
@@ -6,4 +6,17 @@ class Formatter {
  static sanitize(string){
   return string.replace(/[^A-Za-z0-9 '-]+/g, '');
  }
+
+ 
+  static titleize(str) {
+    const alwaysLowerCase = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+    return words.map((word, index) => {
+      if (index === 0 || !alwaysLowerCase.includes(word.toLowerCase())) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      } else {
+        return word.toLowerCase();
+      }
+    }).join(' ');
+  }
 }
